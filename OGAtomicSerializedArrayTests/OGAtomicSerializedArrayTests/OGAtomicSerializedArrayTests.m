f4072fa4bfbfa3d8caebcdb532595945d5991924
Art Gillespie <art@tapsquare.com>
2012-12-03 11:19:02 -0700

    First test.

--- OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests.m
+++ OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests.m
@@ -9,10 +9,20 @@
 #import "OGAtomicSerializedArrayTests.h"
 #import "OGAtomicSerializedArray.h"
 
+NSURL *applicationSupportURL() {
+    NSArray *pathURLs = [[NSFileManager defaultManager] URLsForDirectory:NSApplicationSupportDirectory inDomains:NSUserDomainMask];
+    NSCAssert(0 != [pathURLs count], @"No urls returned for application support directory");
+    NSURL *pathURL = pathURLs[0];
+    [[NSFileManager defaultManager] createDirectoryAtURL:pathURL withIntermediateDirectories:YES attributes:nil error:nil];
+    return pathURL;
+}
+
 @implementation OGAtomicSerializedArrayTests
 
 - (void)testSerialization {
-    GHFail(@"testSerialization unimplmented...");
+    NSString *arrayPath = [[applicationSupportURL() path] stringByAppendingPathComponent:@"test.array"];
+    OGAtomicSerializedArray *array = [OGAtomicSerializedArray atomicSerializedArrayWithPath:arrayPath];
+    GHAssertTrue(nil != array, @"couldn't create atomic array for path: %@", arrayPath);
 }
 
 @end
