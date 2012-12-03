676855f0888bdaadbc78fc78066f4f9c3c393760
Art Gillespie <art@tapsquare.com>
2012-12-03 11:39:22 -0700

    Flesh out test case.

--- OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests.m
+++ OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests.m
@@ -17,12 +17,36 @@ NSURL *applicationSupportURL() {
     return pathURL;
 }
 
+NSString *testPath() {
+    return [[applicationSupportURL() path] stringByAppendingPathComponent:@"test.array"];
+}
+
 @implementation OGAtomicSerializedArrayTests
 
+- (void)setUp {
+
+}
+
+- (void)tearDown {
+    // get rid of the serialized file
+    [OGAtomicSerializedArray purgeAtomicSerializedArrayAtPath:testPath() purgeFromDisk:YES];
+}
+
 - (void)testSerialization {
-    NSString *arrayPath = [[applicationSupportURL() path] stringByAppendingPathComponent:@"test.array"];
-    OGAtomicSerializedArray *array = [OGAtomicSerializedArray atomicSerializedArrayWithPath:arrayPath];
-    GHAssertTrue(nil != array, @"couldn't create atomic array for path: %@", arrayPath);
+    OGAtomicSerializedArray *array = [OGAtomicSerializedArray atomicSerializedArrayWithPath:testPath()];
+    GHAssertNotNil(array, @"couldn't create atomic array for path: %@", testPath());
+    [array addObjectAndSerialize:@"String One"];
+    GHAssertEquals((NSUInteger)1, [array count], @"Expected array count of 1, got: %d", [array count]);
+    GHAssertEqualStrings(@"String One",[array objectAtIndex:0], @"Expected 'String One' in index [0]");
+
+    // we're violating the black box (and atomicity) here by checking the serialized array directly
+    // don't do this in normal usage. We're just testing to ensure the array was written out
+    // correctly.
+    GHAssertTrue([[NSFileManager defaultManager] fileExistsAtPath:testPath()], @"No serialized array at: %@", testPath());
+    NSArray *fileArray = [NSArray arrayWithContentsOfFile:testPath()];
+    GHAssertNotNil(fileArray, @"Couldn't load array from file");
+    GHAssertEquals((NSUInteger)1, [fileArray count], @"Expected 1 element in fileArray");
+    GHAssertEqualStrings(@"String One", [array objectAtIndex:0], @"Expected 'String One' in index [0] of fileArray");
 }
 
 @end
