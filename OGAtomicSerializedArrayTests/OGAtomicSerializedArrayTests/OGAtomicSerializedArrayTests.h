375f363c6a9470abe1640fe41cb669d5aa6ea8ca
Art Gillespie <art@tapsquare.com>
2012-12-03 11:03:25 -0700

    Configure GHUnit.

---
+++ OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests.h
@@ -0,0 +1,13 @@
+//
+//  OGAtomicSerializedArrayTests.h
+//  OGAtomicSerializedArrayTests
+//
+//  Created by Art Gillespie on 12/3/12.
+//  Copyright (c) 2012 Origami Labs. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+
+@interface OGAtomicSerializedArrayTests : GHTestCase
+
+@end
---
+++ OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests/OGAtomicSerializedArrayTests.m
@@ -0,0 +1,18 @@
+//
+//  OGAtomicSerializedArrayTests.m
+//  OGAtomicSerializedArrayTests
+//
+//  Created by Art Gillespie on 12/3/12.
+//  Copyright (c) 2012 Origami Labs. All rights reserved.
+//
+
+#import "OGAtomicSerializedArrayTests.h"
+#import "OGAtomicSerializedArray.h"
+
+@implementation OGAtomicSerializedArrayTests
+
+- (void)testSerialization {
+    GHFail(@"testSerialization unimplmented...");
+}
+
+@end
